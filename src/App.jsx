2904fff4721977a983b973304be3700bbef76476
MateMoscoloni <91292509+MateMoscoloni@users.noreply.github.com>
2022-05-31 21:23:54 -0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './components/NavBar/NavBar'
+import NavBar from './components/NavBar/NavBar';
+
+
+
+function App() {
+  return (
+    <>
+    <NavBar />
+    </>
+    
+  );
+}
+
+export default App;
